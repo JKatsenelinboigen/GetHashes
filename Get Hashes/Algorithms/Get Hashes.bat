cca68e0b7df1618c4d26c5a9c4f9c9d1b9452122
JKatsenelinboigen <43592277+JKatsenelinboigen@users.noreply.github.com>
2019-01-19 20:39:07 -0500

    Added all hashes send

---
+++ Get Hashes/Algorithms/Get Hashes.bat
@@ -0,0 +1,15 @@
+@echo off
+echo Path: "%~dpnx1"
+echo:
+
+echo MD5:
+CertUtil -hashfile "%~dpnx1" MD5 | find /i /v "md5" | find /i /v "certutil"
+echo: 
+
+echo SHA1:
+CertUtil -hashfile "%~dpnx1" SHA1 | find /i /v "SHA1" | find /i /v "certutil"
+echo:
+
+echo SHA256:
+CertUtil -hashfile "%~dpnx1" SHA256 | find /i /v "SHA256" | find /i /v "certutil"
+@pause
